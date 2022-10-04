e8709b972e5ba41b616f614eae80bd92b9570ef8
Dharmendrasinh Chudasama <39006384+dpchudasama2@users.noreply.github.com>
2022-10-04 13:51:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,17 +5,22 @@
 # sudo chmod +777 script.sh
 # sudo sh ./script.sh
 
+function downloadJdk(){
+  echo "Downloading and Installing JDK..."
+  wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
+  tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
+  mv jdk-13.0.1 /opt/
+}
 
-echo "Downloading and Installing JDK..."
-wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
-tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
-mv jdk-13.0.1 /opt/
+function configJdkEnv(){
+  echo "Setting JDK Env."
+  echo 'JAVA_HOME="/opt/jdk-13.0.1"' >> ~/.bashrc
+  echo 'export PATH="$PATH:$JAVA_HOME/bin"' >> ~/.bashrc
+}
 
-echo "Setting JDK Env."
-echo 'JAVA_HOME="/opt/jdk-13.0.1"' >> ~/.bashrc
-echo 'export PATH="$PATH:$JAVA_HOME/bin"' >> ~/.bashrc
-
-sh ~/.bashrc
+function loadEnv(){
+  sh ~/.bashrc
+}
 
 echo "Verify java version"
 java -version
