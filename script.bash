c89a26d4963398e054a619eb023a1d7ade49c8c6
Dharmendrasinh Chudasama <39006384+dpchudasama2@users.noreply.github.com>
2022-10-07 13:31:59 +0530

    Update script.bash

--- script.bash
+++ script.bash
@@ -10,7 +10,7 @@ else
     echo "Downloading and Installing JDK..."
     wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
     tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
-    mv jdk-13.0.1 /opt/
+    sudo mv jdk-13.0.1 /opt/
 
     echo "Setting JDK Env."
     echo 'JAVA_HOME="/opt/jdk-13.0.1"' >> ~/.bashrc
@@ -25,7 +25,7 @@ else
     echo "Downloading and Installing Maven..."
     wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
     tar -xvf apache-maven-3.6.3-bin.tar.gz
-    mv apache-maven-3.6.3 /opt/
+    sudo mv apache-maven-3.6.3 /opt/
 
     echo "Setting Maven Env."
     echo 'M2_HOME="/opt/apache-maven-3.6.3"' >> ~/.bashrc
